a3c6ae9a6b9b7d14daa0de93f3cb679c924a3f12
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-11-19 23:53:14 +0100

    func to handle albums

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from PIL import ImageFile, Image
+from Album import Album
 
 def scaleImage(im: ImageFile, heightTo: int) -> Image:
     
@@ -15,3 +16,25 @@ class COLOR_PALETTE:
     BG_COLOR = "#2E8B57"
     ACCENT_1 = "#61D918"
     
+    
+
+def read_albums(fileStr: str) -> list[Album]:
+    with open(fileStr, "r") as file:
+        lines = file.readlines()
+        
+        albums = []
+        
+        curr_alb = []
+        
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                artist, album, songsNumber, year, downloadNumber = curr_alb
+        
+                albums.append(Album(artist, album, songsNumber, year, downloadNumber))
+                curr_alb = []
+                continue
+        
+            
+            curr_alb.append(line)
+        return albums
