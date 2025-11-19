30b031a45a2068bba121d2864cbc0001202de84e
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-11-20 00:39:02 +0100

    optional imageUrl

--- src/Album.py
+++ src/Album.py
@@ -1,8 +1,10 @@
 class Album:
-    def __init__(self, artist, album, songsNumber, year, downloadNumber):
+    def __init__(self, artist, album, songsNumber, year, downloadNumber, imageUrl = None):
         self.artist = artist
         self.album = album
         self.songsNumber = songsNumber
         self.year = year
         self.downloadNumber = int(downloadNumber)
         
+        
+        self.imageUrl = imageUrl
