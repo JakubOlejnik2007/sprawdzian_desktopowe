0a12684488a7142fb60fa875e09b506ebca59330
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-11-19 23:53:21 +0100

    bold font

--- src/fonts.py
+++ src/fonts.py
@@ -3,4 +3,7 @@ from tkinter import font
 def get_fonts(root):
     ITALIC = font.Font(root=root, family="Arial", size=22, slant="italic")
     BIG_FONT = font.Font(root=root, family="Arial", size=40)
-    return ITALIC, BIG_FONT
+    BOLD_20 = font.Font(root=root, family="Arial", 
+                        #size=20, 
+                        weight="bold")
+    return ITALIC, BIG_FONT, BOLD_20
